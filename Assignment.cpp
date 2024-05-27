0318200338232001860d55becd03226c75d8a4c8
1211209221 <1211209221@student.mmu.edu.my>
2024-05-27 12:01:20 +0800

    error \240

--- Assignment.cpp
+++ Assignment.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <fstream>
 #include <algorithm>
+#include <vector> 
 #include <unistd.h>
 using namespace std;
 class Menus
@@ -24,14 +25,14 @@ class Menus
 			    }
 			    countFile.close();
 			} else {
-			    cout << "Error: Unable to open the file 'records/books.txt'\n";
+			    cout << "Error: Unable to open the file 'books.txt'\n";
 			    exit(0);
 			}
 		    countFile.close();
-
+		    
+			//Display List of Books
 			ifstream list ("books.txt");
 			cout << left << setw(5) << "No." << left << setw(35) << "Book Name" << left << setw(13) << "Price" << left << setw(7) << "Stock" << left << setw(25) << "Author" << left << setw(15) << "Genre" << endl;
-			//List Books
 			while(list >> id >> name >> price >> stock >> author >> genre)
 			{
 				replace(name.begin(), name.end(), '%', ' ');
@@ -52,13 +53,47 @@ class Menus
 			cout << endl << "How do you like to sort the list?" << endl;
 			cout << "1. Sort by Price" << endl;
 			//FOR MING DA
-			cout << "2. Search by " << endl;
+			cout << "2. Sort by " << endl;
 			cout << "3. Back to Main Menu\n";
 			cout << "Enter your choice: ";
 			cin >> choice;
 			if (choice=="1")
 			{
-				//SORT BY PRICE
+				//SORT BY PRICE USING BUCKET SORT
+				//Get number of entries
+				int i=0, numEntries, stock;
+				double price;
+				string line, id, name, author, genre;
+				ifstream countFile("books.txt");
+				if (countFile.is_open())
+				{
+				    while (getline(countFile, line))
+					{
+				        numEntries++;
+				    }
+				    countFile.close();
+				} else {
+				    cout << "Error: Unable to open the file 'books.txt'\n";
+				    exit(0);
+				}
+			    countFile.close();
+			    double arr[numEntries];
+			    //Insert into array
+			    ifstream list ("books.txt");
+				while(list >> id >> name >> price >> stock >> author >> genre)
+				{
+					arr[i] = price;
+					i++;
+				}
+				list.close();
+				int n = sizeof(arr) / sizeof(arr[0]);
+				bucketSort(arr,n);
+				cout << "Sorted array: \n"; 
+			    cout << "-------------" << endl; 
+			    for (int i = 0; i < n; i++)
+				{ 
+			        cout << arr[i] << " "; 
+			    } 
 			}
 			else if (choice=="2")
 			{
@@ -79,6 +114,49 @@ class Menus
 				SortMenus();
 			}
 		}
+		
+		// Insertion sort function to sort individual buckets 
+		void insertionSort(vector<float>& bucket)
+		{ 
+			for (int i = 1; i < bucket.size(); ++i)
+			{ 
+        		float key = bucket[i]; 
+        		int j = i - 1; 
+		        while (j >= 0 && bucket[j] > key)
+				{ 
+		            bucket[j + 1] = bucket[j]; 
+		            j--; 
+		        } 
+		        bucket[j + 1] = key; 
+		    } 
+		} 
+
+		// Function to sort arr[] of size n using bucket sort 
+		void bucketSort(float arr[], int n)
+		{ 
+		    // 1) Create n empty buckets 
+		    vector<float> b[n]; 
+		    // 2) Put array elements in different buckets 
+		    for (int i = 0; i < n; i++)
+			{ 
+		        int bi = n * arr[i]; 
+		        b[bi].push_back(arr[i]); 
+		    } 
+		    // 3) Sort individual buckets using insertion sort 
+		    for (int i = 0; i < n; i++)
+			{ 
+		        insertionSort(b[i]); 
+		    } 
+		    // 4) Concatenate all buckets into arr[] 
+		    int index = 0; 
+		    for (int i = 0; i < n; i++) 
+			{ 
+		        for (int j = 0; j < b[i].size(); j++)
+				{ 
+		            arr[index++] = b[i][j]; 
+		        }
+		    }
+		} 
 };
 
 int main(){
